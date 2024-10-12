1aad0c46f6e83a56cfa28b5a87ce87e288d8ecee
Kevin Saephanh <Kevin.Saephanh@gmail.com>
2024-10-11 20:10:35 -0500

    Revert "Switch to OOP"
    This reverts commit 82e623b55be5ec2fc81801f190af20fa07fb0114.

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from fastapi import FastAPI
 from logger import logger
 from stocks import router
 from .config.config import settings
+from .stocks.stock_producer import shutdown_producer
 
 app = FastAPI()
+
 app.include_router(router)
 
 
@@ -12,6 +14,12 @@ app.include_router(router)
 async def health():
     """Checks health of application"""
     return {"health": 200}
+
+
+@app.on_event("shutdown")
+def shutdown_event():
+    """Flush and close the producer when the app shuts down."""
+    shutdown_producer()
 
 
 if __name__ == "__main__":
--- stocks/router.py
+++ stocks/router.py
@@ -1,29 +1,30 @@
 import asyncio
 from fastapi import APIRouter, HTTPException
-from service import Timeframe, StockService
+from service import Timeframe, run_etl_for_stock
 from logger import logger
 
 
 router = APIRouter(tags=["Stocks"])
-stock_service = StockService()
 
 
-@router.post("/stock-data/{symbol}/{timeframe}")
-def run_etl(symbol: str, timeframe: Timeframe):
+@router.post("/{symbol}/{timeframe}")
+def run_etl(symbol: str, timeframe: Timeframe) -> any:
+    """Fetch historical data for stock ticker symbol and publish to Kafka"""
     try:
-        return stock_service.run_etl_for_stock(symbol, timeframe)
+        return run_etl_for_stock(symbol, timeframe)
     except HTTPException as e:
         logger.error("Error occurred: %s", e)
         raise e
 
 
 @router.post("/bulk-etl")
-async def run_bulk_etl(symbols: list):
+async def run_bulk_etl(symbols: list) -> any:
+    """Fetch historical data for list of stocks and publish to Kafka"""
     if len(list) > 5:
         return {"status": 400, "message": "Stock querying limited to 5 symbols"}
     try:
         # Create a list of tasks for each symbol and timeframe
-        tasks = [stock_service.run_etl_for_stock(sym, t) for sym in symbols for t in Timeframe]
+        tasks = [run_etl_for_stock(sym, t) for sym in symbols for t in Timeframe]
         # Run tasks concurrently
         await asyncio.gather(*tasks)
         return {
