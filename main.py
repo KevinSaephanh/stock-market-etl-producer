b0d4172357163ab37941e2b1a5b7f092d5869abc
Kevin Saephanh <kevinsaephanh@Kevins-Laptop.local>
2024-12-07 17:14:42 -0600

    Add lifespan to app

--- main.py
+++ main.py
@@ -1,28 +1,21 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from logger import logger
 from stocks import router
 from stocks.stock_producer import shutdown_producer
 
-app = FastAPI()
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("App started")
+    yield
+    shutdown_producer()
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(router)
-
-
-@app.on_event("startup")
-async def startup_event():
-    """Log that the application has started"""
-    logger.info("FastAPI application has started")
 
 
 @app.get("/health")
 async def health():
-    """Checks health of application"""
     return {"status": "ok"}
-
-
-@app.on_event("shutdown")
-def shutdown_event():
-    """Flush and close the producer when the app shuts down."""
-    shutdown_producer()
-
-
